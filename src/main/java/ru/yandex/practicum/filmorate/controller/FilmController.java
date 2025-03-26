ed38c547409bb96d66e00741d15b23e84a4fa6b4
vizill <l.bolshe02@gmail.com>
2025-03-26 17:06:53 +0900

    попытка 1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,42 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private List<Film> films = new ArrayList<>();
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        log.info("Добавлен фильм: {}", film.getName());
+        films.add(film);
+        return film;
+    }
+
+    @PutMapping("/{id}")
+    public Film updateFilm(@PathVariable int id, @Valid @RequestBody Film film) {
+        log.info("Обновлен фильм с id: {}", id);
+        Film updatedFilm = films.stream()
+                .filter(f -> f.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ValidationException("Фильм не найден"));
+        updatedFilm.setName(film.getName());
+        updatedFilm.setDescription(film.getDescription());
+        updatedFilm.setReleaseDate(film.getReleaseDate());
+        updatedFilm.setDuration(film.getDuration());
+        return updatedFilm;
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return films;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        log.info("Создан пользователь: {}", user.getLogin());
+        users.add(user);
+        return user;
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable int id, @Valid @RequestBody User user) {
+        log.info("Обновлен пользователь с id: {}", id);
+        User updatedUser = users.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ValidationException("Пользователь не найден"));
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setLogin(user.getLogin());
+        updatedUser.setName(user.getName());
+        updatedUser.setBirthday(user.getBirthday());
+        return updatedUser;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return users;
+    }
+}

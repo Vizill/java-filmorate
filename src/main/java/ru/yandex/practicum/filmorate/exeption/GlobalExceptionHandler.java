33dc5e742935d8dfceec698c7d5d0dfbb6183963
vizill <l.bolshe02@gmail.com>
2025-03-26 17:18:55 +0900

    попытка 1(исправленное 2)

--- src/main/java/ru/yandex/practicum/filmorate/exeption/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.exception;
+package ru.yandex.practicum.filmorate.exeption;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

aeade0e083eb90e16eb1ee54bd67935bc146a6af
vizill <l.bolshe02@gmail.com>
2025-03-27 17:02:37 +0900

    попытка 2

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,53 +1,50 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.exeption.ValidationException;
 
-
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
-    private List<Film> films = new ArrayList<>();
+    private final List<Film> films = new ArrayList<>();
     private int idCounter = 1;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Film createFilm(@RequestBody @Valid Film film) {
+    public ResponseEntity<Film> createFilm(@RequestBody @Valid Film film) {
         film.setId(idCounter++);
         films.add(film);
-        return film;
+        return ResponseEntity.status(HttpStatus.CREATED).body(film);
     }
 
     @GetMapping
-    public List<Film> getFilms() {
-        return films;
+    public ResponseEntity<List<Film>> getFilms() {
+        return ResponseEntity.ok(films);
     }
 
     @PutMapping
-    public Film updateFilm(@RequestBody @Valid Film film) {
+    public ResponseEntity<Film> updateFilm(@RequestBody @Valid Film film) {
         if (film.getId() <= 0) {
             throw new ValidationException("ID фильма должен быть больше 0.");
         }
 
-        Optional<Film> existingFilm = films.stream()
-                .filter(f -> f.getId() == film.getId())
-                .findFirst();
-
-        if (existingFilm.isPresent()) {
-            films.remove(existingFilm.get());
-            films.add(film);
-            return film;
-        } else {
-            throw new ValidationException("Фильм с ID " + film.getId() + " не найден.");
+        for (Film f : films) {
+            if (f.getId() == film.getId()) {
+                f.setName(film.getName());
+                f.setDescription(film.getDescription());
+                f.setReleaseDate(film.getReleaseDate());
+                f.setDuration(film.getDuration());
+                return ResponseEntity.ok(f);
+            }
         }
+
+        throw new ValidationException("Фильм с ID " + film.getId() + " не найден.");
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,52 +1,56 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.exeption.ValidationException;
 
-
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private List<User> users = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
     private int idCounter = 1;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@RequestBody @Valid User user) {
+    public ResponseEntity<User> createUser(@RequestBody @Valid User user) {
         user.setId(idCounter++);
+
+        // Если имя пустое, то устанавливаем его равным логину
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
         users.add(user);
-        return user;
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @GetMapping
-    public List<User> getUsers() {
-        return users;
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(users);
     }
 
     @PutMapping
-    public User updateUser(@RequestBody @Valid User user) {
+    public ResponseEntity<User> updateUser(@RequestBody @Valid User user) {
         if (user.getId() <= 0) {
             throw new ValidationException("ID пользователя должен быть больше 0.");
         }
 
-        Optional<User> existingUser = users.stream()
-                .filter(u -> u.getId() == user.getId())
-                .findFirst();
-
-        if (existingUser.isPresent()) {
-            users.remove(existingUser.get());
-            users.add(user);
-            return user;
-        } else {
-            throw new ValidationException("Пользователь с ID " + user.getId() + " не найден.");
+        for (User u : users) {
+            if (u.getId() == user.getId()) {
+                u.setEmail(user.getEmail());
+                u.setLogin(user.getLogin());
+                u.setName(user.getName().isBlank() ? user.getLogin() : user.getName());
+                u.setBirthday(user.getBirthday());
+                return ResponseEntity.ok(u);
+            }
         }
+
+        throw new ValidationException("Пользователь с ID " + user.getId() + " не найден.");
     }
 }

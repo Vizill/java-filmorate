e1944d025801543213c82ab85e62fda169475207
vizill <l.bolshe02@gmail.com>
2025-03-28 12:01:39 +0900

    попытка 3

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,15 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import ru.yandex.practicum.filmorate.validation.ReleaseDateConstraint;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
-@Setter
-@Getter
 @Data
 public class Film {
     private int id;
@@ -24,7 +20,7 @@ public class Film {
     @ReleaseDateConstraint
     private LocalDate releaseDate;
 
-    @Positive(message = "Продолжительность фильма должна быть положительным числом.")
+    @Min(value = 1, message = "Продолжительность фильма должна быть положительной")
     private int duration;
 
     public Film(int id, String name, String description, LocalDate releaseDate, Integer duration) {
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 

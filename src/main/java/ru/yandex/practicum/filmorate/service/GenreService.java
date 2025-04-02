7e857780f730dadf6f0f510c778d25c0b30a6e76
vizill <l.bolshe02@gmail.com>
2025-04-02 17:40:45 +0900

    попытка 2(всё ещё нужна помощь)

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exeption.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
